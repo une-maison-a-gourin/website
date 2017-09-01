c5fdc25f65ce968c8278e2b902fc1434b4e0ef4e
Gourin Maison <31484686+une-maison-a-gourin@users.noreply.github.com>
2017-09-01 21:48:31 +0100

    Converting index2.html to use brunch [WIP]

--- brunch-config.js
+++ brunch-config.js
@@ -10,4 +10,24 @@ exports.files = {
 };
 
 exports.plugins = {
+  babel: {presets: ['latest']}
+};
+
+exports.modules = {
+  autoRequire: {
+    'app.js': ['jQuery', 'popper', 'bootstrap', 'jquery.easing', "scrollreveal", "magnific-popup"]
+  },
+};
+
+exports.npm = {
+  globals: {
+    jQuery: 'jquery',
+    $: 'jquery',
+    bootstrap: 'bootstrap',
+  },
+  styles: {
+    bootstrap: ['dist/css/bootstrap.css'],
+    "font-awesome": ['css/font-awesome.css'],
+    "magnific-popup": ['dist/magnific-popup.css'],
+  },
 };
