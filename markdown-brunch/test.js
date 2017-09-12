6e8d33f199e74d85d8d3dc165846dedc15663379
Gourin Maison <31484686+une-maison-a-gourin@users.noreply.github.com>
2017-09-12 16:03:07 +0100

    Added own markdown-it brunch static asset compile plugin

---
+++ markdown-brunch/test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const expect = require('chai').expect;
+const Plugin = require('./');
+
+describe('Plugin', () => {
+  let plugin;
+
+  beforeEach(() => {
+    plugin = new Plugin({paths: {root: '.'}});
+  });
+
+  it('should be an object', () => {
+    expect(plugin).to.be.ok;
+  });
+
+  it('should has #compileStatic method', () => {
+    expect(plugin.compileStatic).to.be.an.instanceof(Function);
+  });
+
+  it('should compileStatic and produce valid result', () => {
+    const content = '# This is a title';
+    var expected = "<h1>This is a title</h1>\n";
+
+    return plugin.compileStatic({data: content, path: ''}).then(got => {
+      expect(got).to.equal(expected);
+    }, error => expect("Got " + got + " " + error).not.to.be.ok);
+  });
+
+});
