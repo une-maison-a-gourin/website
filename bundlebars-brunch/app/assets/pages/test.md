db6efae0ec00c726a0d4448b5422afd7befab0f0
Gourin Maison <31484686+une-maison-a-gourin@users.noreply.github.com>
2017-09-13 17:06:09 +0100

    Created bundle bars with markdown-it plugin

---
+++ bundlebars-brunch/app/assets/pages/test.md
@@ -0,0 +1,35 @@
+---
+title: Just hack'n
+description: Nothing to see here
+template: landing.html
+head:
+    title: Hi
+    styles:
+      -
+        href: style.css
+footer:
+  companyName: "Copyleft"
+logo:
+  href: "index.html"
+  url: "img/logo.svg"
+nav:
+  items:
+    -
+      title: "Home"
+      href: "index.html"
+    -
+      title: "About"
+      href: "about.html"
+---
+
+# {{title}} 
+
+And this is the body of the content with the description of 
+
+{{description}} 
+
+inserted by bundlebars and also a partial of 
+
+{{> apart}}
+
+inserted.
---
+++ bundlebars-brunch/index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const path = require('path');
+const Bundlebars = require('bundlebars');
+const fm = require('front-matter');
+const Readable = require('stream').Readable;
+const md = require('markdown-it')({
+  html: true,
+  linkify: true,
+  typographer: true,
+});
+
+class BundlebarsCompiler {
+  constructor(config) {
+    if (config.plugins) {
+      this.config = config.plugins.BundlebarsCompiler;
+    }
+    this.config = {
+      partialsDir: './app/assets/pages/partials',
+      partialsExt: '.html',
+      helpersDir: './app/assets/pages/helpers',
+      templatesDir: './app/assets/pages/templates',
+    };
+    this.bundlebars = new Bundlebars(this.config);
+  }
+
+  toStream(str) {
+    var s = new Readable();
+    s._read = function() {};
+    s.push(str);
+    s.push(null);
+    return s;
+  }
+
+  onError(err) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  compileBundle(stream, data) {
+    return this.bundlebars.compile(stream, data);
+  }
+
+  compileStatic(params) {
+    var dataContent = fm(params.data);
+    var content;
+    var me = this;
+    var body = me.compileBundle(this.toStream(dataContent.body), dataContent.attributes);
+    if (dataContent.attributes.template) {
+      var file = path.join(this.config.templatesDir, dataContent.attributes.template);
+      content = body.then(function(data) {
+        dataContent.attributes.content = data;
+        return me.compileBundle(file, dataContent.attributes);
+      }, this.onError);
+    } else {
+      content = body;
+    }
+    return content.then(function(compiled) {
+      return new Promise((resolve, reject) => {
+        if (compiled === '') {
+          reject('Empty data');
+        } else {
+          resolve(md.render(compiled));
+        }
+      });
+    }, this.onError);
+  }
+}
+
+BundlebarsCompiler.prototype.brunchPlugin = true;
+BundlebarsCompiler.prototype.type = 'template';
+BundlebarsCompiler.prototype.extension = 'md';
+BundlebarsCompiler.prototype.staticTargetExtension = 'html';
+
+module.exports = BundlebarsCompiler;
