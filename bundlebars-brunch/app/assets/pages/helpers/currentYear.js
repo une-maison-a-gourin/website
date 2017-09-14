8a78da8a927f974f15577be407061c8a042ef6b7
Gourin Maison <31484686+une-maison-a-gourin@users.noreply.github.com>
2017-09-14 15:52:37 +0100

    Replaced local bundlebars-brunch plugin which has been moved to git as brunch-jekyllish-plugin

--- bundlebars-brunch/app/assets/pages/helpers/currentYear.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (Handlebars) {
-  Handlebars.registerHelper('currentYear', function () {
-    return (new Date()).getFullYear();
-  });
-
-  Handlebars.registerHelper('log', function (a) {
-    console.log(a);
-  });
-};
